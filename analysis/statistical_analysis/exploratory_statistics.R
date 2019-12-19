b4bbf3914642bbfb04e663e69376369bdc351a13
blandolt <balduin.landolt@unibas.ch>
2019-12-20 00:04:55 +0100

    Feature (statistics): add some plots to exploratory statistics. + remove some old code

--- analysis/statistical_analysis/exploratory_statistics.R
+++ analysis/statistical_analysis/exploratory_statistics.R
@@ -3,7 +3,7 @@
 
 
 # set working directory
-setwd()
+# setwd() # adjust to set working dir
 getwd()
 
 # load package
@@ -13,27 +13,43 @@ library(ggplot2)
 # read data from file
 page_overview = read.csv("../tmp_data/page_overview.csv")
 head(page_overview)
+data_by_line = read.csv("../tmp_data/data_by_line.csv")
+head(data_by_line)
 
 
 page_overview$s_fact = as.factor(page_overview$sample)
 page_overview$abbreviations_per_word = page_overview$no_abbreviations/page_overview$no_words
 page_overview$abbreviations_per_line = page_overview$no_abbreviations/page_overview$no_lines
 page_overview$abbreviations_per_page = page_overview$no_abbreviations/page_overview$no_pages
+page_overview$abbreviations_per_100chars = page_overview$no_abbreviations/page_overview$no_characters*1000
 page_overview$lines_per_page = page_overview$no_lines/page_overview$no_pages
-
-
-plot(page_overview$s_fact, page_overview$no_abbreviations)
-plot(page_overview$no_abbreviations)
-hist(page_overview$no_abbreviations, breaks = 10)
+page_overview$chars_per_page = page_overview$no_characters/page_overview$no_pages
+page_overview$chars_per_line = page_overview$no_characters/page_overview$no_lines
 
 
 
-qplot(page_overview$no_abbreviations)
-qplot(s_fact, lines_per_page, data = page_overview)
-qplot(no_abbreviations, data = page_overview, geom = "density")
+# plot words per line
+ggplot(data_by_line, aes(data_by_line$no_words)) + geom_histogram(fill="red")
+qplot(data_by_line$no_words, geom="histogram", fill=factor(data_by_line$sample))
+ggplot(data_by_line, aes(data_by_line$no_words, fill=factor(data_by_line$sample))) + geom_histogram(position = "dodge")
+ggplot(data_by_line, aes(y=data_by_line$no_words, x=factor(data_by_line$sample))) + geom_boxplot()
+
+
+# plot characters per line
+qplot(data_by_line$no_characters, geom="histogram", fill=factor(data_by_line$sample))
+ggplot(data_by_line, aes(data_by_line$no_characters, fill=factor(data_by_line$sample))) + geom_histogram(position = "dodge")
+ggplot(data_by_line, aes(y=data_by_line$no_characters, x=factor(data_by_line$sample))) + geom_boxplot()
+
+
+# plot abbreviations per line
+qplot(data_by_line$no_abbreviations, geom="histogram", fill=factor(data_by_line$sample))
+ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
+  geom_histogram(position = "dodge", bins = max(data_by_line$no_abbreviations))
+ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
+  geom_density(fill="red", adjust=0.65) 
+ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
+  geom_density(alpha=0.5, adjust=0.65) 
+ggplot(data_by_line, aes(y=data_by_line$no_abbreviations, x=factor(data_by_line$sample))) + geom_boxplot()
 
 
 
-#obsolete
-page_overview_transposed = as.data.frame(t(page_overview), row.names=page_overview$informations)
-head(page_overview_transposed)
