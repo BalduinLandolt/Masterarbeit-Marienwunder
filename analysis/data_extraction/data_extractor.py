10917b494e62b957b913d6883aa2c6c627332350
blandolt <balduin.landolt@unibas.ch>
2019-12-22 19:18:47 +0100

    Docs (extraction): add docstrings for most methods

--- analysis/data_extraction/data_extractor.py
+++ analysis/data_extraction/data_extractor.py
@@ -12,6 +12,10 @@ import csv
 
 
 class Extractor:
+    """
+    Class that organized extraction of data from XML.
+    """
+
     TYPE_EXTRACT_ALL = 'all'
     TYPE_EXTRACT_EX = 'ex'
     TYPE_EXTRACT_AM = 'am'
@@ -20,10 +24,12 @@ class Extractor:
 
     @staticmethod
     def extract():
-        """
+        """ Extract everything.
+
         convenience method that calls all extractions that are implemented.
 
-        :return: None
+        Returns:
+            None: None
         """
 
         # load xml file
@@ -67,13 +73,13 @@ class Extractor:
         # get words abbreviation-only
         words_raw_rep_am_only = Extractor.get_words_raw_rep(xml_soup, Extractor.TYPE_EXTRACT_AM)
 
-        abbreviation_marks = Extractor.get_abbreviation_marks(xml_soup)
+        abbreviation_marks = Extractor.get_abbreviation_marks_raw(xml_soup)
         abbreviation_mark_frequencies = Extractor.get_abbreviation_mark_frequencies(xml_soup)
 
-        expansions = Extractor.get_expansions(xml_soup)
+        expansions = Extractor.get_expansions_raw(xml_soup)
         expansion_frequencies = Extractor.get_expansion_frequencies(xml_soup)
 
-        abbreviations = Extractor.get_abbreviations(xml_soup)
+        abbreviations = Extractor.get_abbreviations_raw(xml_soup)
         abbreviation_frequencies = Extractor.get_abbreviation_frequencies(xml_soup)
 
         # Data export to CSV
@@ -100,11 +106,28 @@ class Extractor:
         # TODO: ...
 
     @staticmethod
-    def get_abbreviation_count(file):
-        return len(file.find_all('abbreviation'))
+    def get_abbreviation_count(data):
+        """Count abbreviations
+
+        Count the number of abbreviations in an XML.
+
+        Args:
+            data (BeautifulSoup): XML data
+
+        Returns:
+            int: Number of abbreviations
+        """
+
+        return len(data.find_all('abbreviation'))
 
     @staticmethod
     def extract_page_overview_info():
+        """
+        Extracts page overview information and writes it to a CSV file.
+
+        Returns:
+            None: None
+        """
         # TODO: could extraction be more generic, and with arguments to specify?
         field_names = ["sample", "sample_name", "no_pages", "no_lines", "no_words", "no_characters", "no_abbreviations"]
         rows = []
@@ -119,6 +142,19 @@ class Extractor:
 
     @staticmethod
     def write_to_csv(file, names, rows):
+        """Writes data to CSV file.
+
+        Writes a given set of data to a local file
+
+        Args:
+            file (str): File name
+            names (list of str): Column names
+            rows (list of list): Rows of data (each row must be a list of same length as `names`)
+
+        Returns:
+            None: None
+        """
+
         path_prefix = "../tmp_data/"
         with open(path_prefix + file, mode='w', encoding='utf-8', newline='') as file:
             w = csv.writer(file)
@@ -128,6 +164,19 @@ class Extractor:
 
     @staticmethod
     def get_word_frequencies(words_raw_rep, plot=False, print_no=0):
+        """Get word frequencies.
+
+        Get a word frequency distribution of a text of words in raw form.
+
+        Args:
+            words_raw_rep (list of str): Words in raw text format.
+            plot (bool): Plot data if True. Default is False.
+            print_no (int): Print most frequent words to console. If 0, nothing is printed.
+
+        Returns:
+            nltk.FreqDist: Frequency distribution of the words.
+        """
+
         frequ = nltk.FreqDist(words_raw_rep)
         if plot:
             frequ.plot()
@@ -139,25 +188,52 @@ class Extractor:
         return frequ
 
     @staticmethod
-    def get_page_count(file):
-        pbs = file.find_all('page')
-        # print(pbs)
-        return len(pbs)
+    def get_page_count(data):
+        """
+        Count number of pages.
+        Args:
+            data (BeautifulSoup): XML data
+
+        Returns:
+            int: Number of pages
+        """
+        return len(data.find_all('page'))
 
     @staticmethod
-    def get_line_count(file):
-        lbs = file.find_all('line')
-        # print(lbs)
-        return len(lbs)
+    def get_line_count(data):
+        """
+        Count number of lines.
+        Args:
+            data (BeautifulSoup): XML data
+
+        Returns:
+            int: Number of lines
+        """
+        return len(data.find_all('line'))
 
     @staticmethod
-    def get_word_count(file):
-        ws = file.find_all('w')
-        # print(ws)
-        return len(ws)
+    def get_word_count(data):
+        """
+        Count number of words.
+        Args:
+            data (BeautifulSoup): XML data
+
+        Returns:
+            int: Number of words
+        """
+        return len(data.find_all('w'))
 
     @staticmethod
     def resolve_glyph(w):
+        """
+        Change all glyphs from xml to raw representation.
+
+        Args:
+            w (Tag): Tag potentially containing a glyph.
+
+        Returns:
+            Tag: copy of the input Tag, where <g> is replaced with raw representation.
+        """
         res = copy.copy(w)
         for glyph in res.find_all('g'):
             val = glyph['ref'][1:]
@@ -167,6 +243,17 @@ class Extractor:
 
     @staticmethod
     def resolve_abbreviations(w, type):
+        """
+        Change all abbreviations from xml to raw representation.
+
+        Args:
+            w (Tag): Tag potentially containing an abbreviation.
+            type (str): defines what information shall be retained.
+                Can be 'am', 'ex' or 'all'.
+
+        Returns:
+            Tag: copy of the input Tag, where <abbreviation> is replaced with raw representation.
+        """
         res = copy.copy(w)
         if res.name == 'abbreviation':
             Extractor.extract_abbreviation_contents(res, type)
@@ -177,6 +264,17 @@ class Extractor:
 
     @staticmethod
     def extract_abbreviation_contents(abbr, type):
+        """
+        Extract contents of a single abbreviation.
+
+        Args:
+            abbr (Tag): the abbreviation
+            type (str): defines what information shall be retained.
+                Can be 'am', 'ex' or 'all'.
+
+        Returns:
+            str: raw representation of the abbreviation.
+        """
         abbr.smooth()
         ex = abbr.ex.string or ''
         infix = abbr.infix.string or ''
@@ -193,6 +291,21 @@ class Extractor:
 
     @staticmethod
     def make_raw(w, type):
+        """Convert XML to raw.
+
+        Resolves glyphs and abbreviations, then extracts text contents.
+        (Seems to work on <w> level or below; longer sequences uncertain.)
+        NB: If it returns None, that is usually, because it gets multiple strings,
+        in which case `Tag.string` returns None.
+
+        Args:
+            w (Tag): XML data
+            type (str): defines what information shall be retained.
+                Can be 'am', 'ex' or 'all'.
+
+        Returns:
+            str: raw representation of XML
+        """
         tmp = Extractor.resolve_glyph(w)
         tmp = Extractor.resolve_abbreviations(tmp, type)
         tmp.smooth()
@@ -200,15 +313,39 @@ class Extractor:
 
     @staticmethod
     def get_words_raw_rep(file, type, replace_wordparts=True):
+        """
+        Gets all words as a list of raw strings.
+
+        Args:
+            file (BeautifulSoup): XML data
+            type (str): defines what information shall be retained.
+                Can be 'am', 'ex' or 'all'.
+            replace_wordparts (bool): if true, wordparts are added to previous words. Default is True.
+                Should be false, if line length is looked at, because wordparts end up in the previous line.
+
+        Returns:
+            list of str: raw words
+        """
         file_tmp = copy.copy(file)
         if replace_wordparts:
             file_tmp = Extractor.replace_wordparts(file)
+        # TODO: if replace_wordparts is False, treat them as words?
+        # TODO: handle <pc>?
         ws = file_tmp.find_all('w')
         rws = [Extractor.make_raw(copy.copy(w), type).replace('\n', '') for w in ws]
         return rws
 
     @staticmethod
     def replace_wordparts(file):
+        """
+        Moves wordparts into previous words.
+
+        Args:
+            file (BeautifulSoup): XML data
+
+        Returns:
+            BeautifulSoup: Copy of input data, where wordparts have been moved to previous words.
+        """
         res = copy.copy(file)
         for wp in res.find_all('wordpart'):
             prev_line = wp.parent.previous_sibling
@@ -223,6 +360,15 @@ class Extractor:
 
     @staticmethod
     def strip_whitespace(xml_soup):
+        """
+        Strip XML data of whitespace-only nodes.
+
+        Args:
+            xml_soup (BeautifulSoup): XML Data
+
+        Returns:
+            BeautifulSoup: input data, without any NavigableString containing only whitespace.
+        """
         for e in xml_soup.descendants:
             if isinstance(e, NavigableString):
                 next = e.next_element
@@ -234,7 +380,16 @@ class Extractor:
         return xml_soup
 
     @classmethod
-    def get_abbreviation_marks(cls, soup):
+    def get_abbreviation_marks_raw(cls, soup):
+        """
+        Get all abbreviation marks as raw.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            list of str: abbreviation marks as raw
+        """
         ams = soup.find_all('am')
         res = []
         for am in ams:
@@ -243,12 +398,30 @@ class Extractor:
 
     @classmethod
     def get_abbreviation_mark_frequencies(cls, soup):
-        ams = cls.get_abbreviation_marks(soup)
+        """
+        Get frequency distribution of abbreviation marks.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            nltk.FreqDist: Frequency distribution of abbreviation marks.
+        """
+        ams = cls.get_abbreviation_marks_raw(soup)
         frequs = cls.get_word_frequencies(ams, print_no=5)
         return frequs
 
     @classmethod
-    def get_expansions(cls, soup):
+    def get_expansions_raw(cls, soup):
+        """
+        Get all expansions as raw.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            list of str: expansions as raw
+        """
         ams = soup.find_all('ex')
         res = []
         for am in ams:
@@ -257,12 +430,30 @@ class Extractor:
 
     @classmethod
     def get_expansion_frequencies(cls, soup):
-        exs = cls.get_expansions(soup)
+        """
+        Get frequency distribution of expansions.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            nltk.FreqDist: Frequency distribution of expansions.
+        """
+        exs = cls.get_expansions_raw(soup)
         frequs = cls.get_word_frequencies(exs, print_no=5)
         return frequs
 
     @classmethod
-    def get_abbreviations(cls, soup):
+    def get_abbreviations_raw(cls, soup):
+        """
+        Get all abbreviations as raw.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            list of str: abbreviations as raw
+        """
         ams = soup.find_all('abbreviation')
         res = []
         for am in ams:
@@ -272,12 +463,29 @@ class Extractor:
 
     @classmethod
     def get_abbreviation_frequencies(cls, soup):
-        abbreviations = cls.get_abbreviations(soup)
+        """
+        Get frequency distribution of abbreviations.
+
+        Args:
+            soup (BeautifulSoup): xml data
+
+        Returns:
+            nltk.FreqDist: Frequency distribution of abbreviations.
+        """
+        abbreviations = cls.get_abbreviations_raw(soup)
         frequs = cls.get_word_frequencies(abbreviations, print_no=5)
         return frequs
 
     @staticmethod
     def extract_data_by_line():
+        """Extract line data.
+
+        Extract number of words, characters and abbreviations for each line.
+        Write data to CSV file.
+
+        Returns:
+            None: None
+        """
         names = ["sample", "sample_name", "line_number", "no_words", "no_characters", "no_abbreviations"]
         rows = []
         for section_index, section in enumerate(Extractor.samples):
@@ -292,12 +500,31 @@ class Extractor:
 
     @classmethod
     def get_lines(cls, data):
+        """
+        Get a list of all lines in a document.
+
+        Args:
+            data (BeautifulSoup): XML data
+
+        Returns:
+            list of Tag: all <line> tags.
+        """
         tmp = copy.copy(data)
         cls.replace_wordparts(tmp)
         return tmp.find_all('line')
 
     @classmethod
     def get_character_count(cls, soup):
+        """Get number of characters in XML.
+
+        Counts all characters in XML data. <g> is counted as 1 character.
+
+        Args:
+            soup (Tag): XML data
+
+        Returns:
+            int: number of characters
+        """
         tmp = copy.copy(soup)
         for g in tmp.find_all('g'):
             g.replace_with('^')
