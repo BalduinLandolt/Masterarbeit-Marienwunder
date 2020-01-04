2601f43e8d58d4fd3e72c2c51c5226c91695d305
blandolt <balduin.landolt@unibas.ch>
2020-01-04 21:30:57 +0100

    Feature (analysis): save plots

--- analysis/statistical_analysis/exploratory_statistics.R
+++ analysis/statistical_analysis/exploratory_statistics.R
@@ -61,55 +61,64 @@ page_overview$chars_per_line = page_overview$no_characters/page_overview$no_line
 
 
 # ensures output folder exists
-if (!dir.exists()){
+if (!dir.exists("../out/plots")){
   dir.create("../out/plots")
+}
+if (!dir.exists("../out/plots/perLine")){
+  dir.create("../out/plots/perLine")
+}
+if (!dir.exists("../out/plots/abbreviations")){
+  dir.create("../out/plots/abbreviations")
+}
+if (!dir.exists("../out/plots/abbreviations/forEach")){
+  dir.create("../out/plots/abbreviations/forEach")
 }
 # NB: to be sure the time stamp of the output plots is correct, make sure to delete all previously existing plots
 
 
 # plot words per line
 plot = ggplot(data_by_line, aes(data_by_line$no_words)) + geom_histogram(fill="red")
-ggsave("../out/plots/hist_wordsPerLine.png", plot = plot)
+ggsave("../out/plots/perLine/hist_wordsPerLine.png", plot = plot)
 
 plot = qplot(data_by_line$no_words, geom="histogram", fill=factor(data_by_line$sample))
-ggsave("../out/plots/hist_wordsPerLine_bySample_stack.png", plot = plot)
+ggsave("../out/plots/perLine/hist_wordsPerLine_bySample_stack.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(data_by_line$no_words, fill=factor(data_by_line$sample))) + geom_histogram(position = "dodge")
-ggsave("../out/plots/hist_wordsPerLine_bySample_dodge.png", plot = plot)
+ggsave("../out/plots/perLine/hist_wordsPerLine_bySample_dodge.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(y=data_by_line$no_words, x=factor(data_by_line$sample))) + geom_boxplot()
-ggsave("../out/plots/box_wordsPerLine_bySample.png", plot = plot)
+ggsave("../out/plots/perLine/box_wordsPerLine_bySample.png", plot = plot)
 
 
 # plot characters per line
 plot = qplot(data_by_line$no_characters, geom="histogram", fill=factor(data_by_line$sample))
-ggsave("../out/plots/hist_charactersPerLine_bySample_stack.png", plot = plot)
+ggsave("../out/plots/perLine/hist_charactersPerLine_bySample_stack.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(data_by_line$no_characters, fill=factor(data_by_line$sample))) + geom_histogram(position = "dodge")
-ggsave("../out/plots/hist_charactersPerLine_bySample_dodge.png", plot = plot)
+ggsave("../out/plots/perLine/hist_charactersPerLine_bySample_dodge.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(y=data_by_line$no_characters, x=factor(data_by_line$sample))) + geom_boxplot()
-ggsave("../out/plots/box_charactersPerLine_bySample.png", plot = plot)
+ggsave("../out/plots/perLine/box_charactersPerLine_bySample.png", plot = plot)
 
 
 # plot abbreviations per line
 plot = qplot(data_by_line$no_abbreviations, geom="histogram", fill=factor(data_by_line$sample))
-ggsave("../out/plots/hist_abbreviationsPerLine_bySample_stack.png", plot = plot)
+ggsave("../out/plots/perLine/hist_abbreviationsPerLine_bySample_stack.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
   geom_histogram(position = "dodge", bins = max(data_by_line$no_abbreviations))
-ggsave("../out/plots/hist_abbreviationsPerLine_bySample_dodge.png", plot = plot)
+ggsave("../out/plots/perLine/hist_abbreviationsPerLine_bySample_dodge.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
   geom_density(fill="red", adjust=0.65)
-ggsave("../out/plots/density_abbreviationsPerLine.png", plot = plot)
+ggsave("../out/plots/perLine/density_abbreviationsPerLine.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(data_by_line$no_abbreviations, fill=factor(data_by_line$sample))) +
   geom_density(alpha=0.5, adjust=0.65) 
-ggsave("../out/plots/density_abbreviationsPerLine_bySample.png", plot = plot)
+ggsave("../out/plots/perLine/density_abbreviationsPerLine_bySample.png", plot = plot)
 
 plot = ggplot(data_by_line, aes(y=data_by_line$no_abbreviations, x=factor(data_by_line$sample))) + geom_boxplot()
-ggsave("../out/plots/box_abbreviationsPerLine_bySample.png", plot = plot)
+ggsave("../out/plots/perLine/box_abbreviationsPerLine_bySample.png", plot = plot)
 
 # test normal distribution
 shapiro.test(data_by_line$no_abbreviations)
@@ -124,26 +133,26 @@ shapiro.test(data_by_line$no_abbreviations[which(data_by_line$sample == 1)])
 # plot abbreviation mark distribution
 plot = ggplot(abbreviations, aes(abbreviations$am))
 plot = plot + geom_bar()
-plot
+ggsave("../out/plots/abbreviations/hist_amDistribution.png", plot = plot, width = 20)
 
 abbr_count = data.frame(table(abbreviations$sample, abbreviations$am, dnn = c("sample", "am")))
 abbr_count$Freq_normalized = mapply(normalize, abbr_count$Freq, abbr_count$sample)
 
 plot = ggplot(abbr_count, aes(abbr_count$am, abbr_count$Freq_normalized, fill=abbr_count$sample))
 plot = plot + geom_bar(position = "dodge", stat = "identity")
-plot
+ggsave("../out/plots/abbreviations/hist_amDistribution_bySample.png", plot = plot, width = 20)
 
 # plot expansion distribution
 plot = ggplot(abbreviations, aes(abbreviations$ex))
 plot = plot + geom_bar()
-plot
+ggsave("../out/plots/abbreviations/hist_exDistribution.png", plot = plot, width = 30)
 
 ex_count = data.frame(table(abbreviations$sample, abbreviations$ex, dnn = c("sample", "ex")))
 ex_count$Freq_normalized = mapply(normalize, ex_count$Freq, ex_count$sample)
 
 plot = ggplot(ex_count, aes(ex_count$ex, ex_count$Freq_normalized, fill=ex_count$sample))
 plot = plot + geom_bar(position = "dodge", stat = "identity")
-plot
+ggsave("../out/plots/abbreviations/hist_exDistribution_bySample.png", plot = plot, width = 30)
 
 
 # look at them separately
@@ -157,7 +166,7 @@ for (val in sub_frame_am){
   plot = plot + geom_bar(position = position_dodge(width = 1), stat = "identity")
   plot = plot + geom_text(aes(label=ex_count$Freq), vjust=-0.3, size=3.5, check_overlap = TRUE, position = position_dodge(width = 1))
   plot = plot + ggtitle(paste("AM: ", levels(fr$am)))
-  print(plot)
+  ggsave(paste("../out/plots/abbreviations/forEach/am_",levels(fr$am),".png", sep = ""), plot = plot)
 }
 
 sub_frame_ex = as.vector(lapply(levels(abbreviations$ex), get_subframe_ex))
@@ -170,7 +179,7 @@ for (val in sub_frame_ex){
   plot = plot + geom_bar(position = position_dodge(width = 1), stat = "identity")
   plot = plot + geom_text(aes(label=am_count$Freq), vjust=-0.3, size=3.5, check_overlap = TRUE, position = position_dodge(width = 1))
   plot = plot + ggtitle(paste("EX: ", levels(fr$ex)))
-  print(plot)
+  ggsave(paste("../out/plots/abbreviations/forEach/ex_",levels(fr$ex),".png", sep = ""), plot = plot)
 }
 
 
