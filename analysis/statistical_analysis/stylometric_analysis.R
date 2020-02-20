195b1e81f1b5b3a05471c1c6c0fe26d5a60833c5
blandolt <balduin.landolt@unibas.ch>
2020-02-20 15:57:06 +0100

    Feature (stylo): add stylo script

---
+++ analysis/statistical_analysis/stylometric_analysis.R
@@ -0,0 +1,46 @@
+# Balduin Landolt, 2019-2020
+# License: AGPL (see file `LICENSE-AGPL` in project root)
+
+
+# set working directory
+# setwd() # adjust to set working dir
+current_working_dir <- dirname(rstudioapi::getActiveDocumentContext()$path) # NB: only works in RStudio!
+setwd(current_working_dir) # Dito!
+getwd()
+
+
+
+# load package
+library(stylo)
+
+
+# manual testing with GUI
+stylo()
+oppose()
+
+
+# load samples
+sample_01 = scan("../tmp_data/stylo/abbr_only/part_01_vol1_p1ff.txt", what="character", sep=" ")
+sample_02 = scan("../tmp_data/stylo/abbr_only/part_02_vol1_p473ff.txt", what="character", sep=" ")
+sample_03 = scan("../tmp_data/stylo/abbr_only/part_03_vol2_p303ff.txt", what="character", sep=" ")
+
+# make sample list
+sample_list = list(sample_01, sample_02, sample_03)
+names(sample_list) = c("sample_01", "sample_02", "sample_03")
+
+# test: run stylo on sample list (props GUI)
+stylo(parsed.corpus = sample_list)
+
+# create frequency list and table
+frequ_list_all = make.frequency.list(sample_list)
+freq_table = make.table.of.frequencies(sample_list, frequ_list_all)
+
+
+# calculate distance matrix
+dist_matrix = dist.cosine(freq_table)
+as.matrix(dist_matrix)
+
+
+
+
+
